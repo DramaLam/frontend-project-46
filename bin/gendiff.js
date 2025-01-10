3ace6844354d299262210b78319f49ced14e0e9a
Milena K <123456milena@rambler.ru>
2025-01-10 15:07:55 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,15 @@
 import { program } from 'commander';
+import gendiff from '../src/index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const gendiff = (filepath1, filepath2) => {
+  const data1 = fs.readFileSync(filepath1, 'utf-8');
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
+
+  const parsedData1 = JSON.parse(data1)
+  const parsedData2 = JSON.parse(data2)
+
+  return {file1: parsedData1, file2: parsedData2};
+};
+
+export default {gendiff};
+//gendiff('file1.json', 'file2.json');
