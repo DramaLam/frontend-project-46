7b616ea9834f9a30578523acb9d3ffdbfcfe0d08
Milena K <123456milena@rambler.ru>
2025-01-10 15:31:17 +0300

    step 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import fs from 'fs';
+import path from 'path';
+import process from 'process';
 
 const gendiff = (filepath1, filepath2) => {
-  const data1 = fs.readFileSync(filepath1, 'utf-8');
-  const data2 = fs.readFileSync(filepath2, 'utf-8');
+  const absolutePath1 = path.resolve(filepath1);
+  const absolutePath2 = path.resolve(process.cwd(), filepath2);
+  console.log(absolutePath1);
+  console.log(absolutePath2);
+
+  const data1 = fs.readFileSync(absolutePath1, 'utf-8');
+  const data2 = fs.readFileSync(absolutePath2, 'utf-8');
 
   const parsedData1 = JSON.parse(data1);
   const parsedData2 = JSON.parse(data2);
@@ -10,5 +17,5 @@ const gendiff = (filepath1, filepath2) => {
   return {file1: parsedData1, file2: parsedData2};
 };
 
-export default { gendiff };
+export default gendiff;
 //gendiff('file1.json', 'file2.json');
