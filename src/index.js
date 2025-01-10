3203d5e3ebb000bb60c00f19d4cdefd949bbc937
Milena K <123456milena@rambler.ru>
2025-01-10 15:58:02 +0300

    step 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import path from 'path';
 import process from 'process';
 
 const gendiff = (filepath1, filepath2) => {
-  const absolutePath1 = path.resolve(filepath1);
+  const absolutePath1 = path.resolve(process.cwd(), filepath1);
   const absolutePath2 = path.resolve(process.cwd(), filepath2);
   console.log(absolutePath1);
   console.log(absolutePath2);
@@ -14,7 +14,7 @@ const gendiff = (filepath1, filepath2) => {
   const parsedData1 = JSON.parse(data1);
   const parsedData2 = JSON.parse(data2);
 
-  return {file1: parsedData1, file2: parsedData2};
+  return { file1: parsedData1, file2: parsedData2 };
 };
 
 export default gendiff;
