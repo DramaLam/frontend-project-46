a07c211d6881bb076a03f8b31bb717891ebb5814
Milena K <123456milena@rambler.ru>
2025-01-21 02:21:24 +0300

    fix mutation

--- src/index.js
+++ src/index.js
@@ -18,16 +18,9 @@ const genDiff = (filepath1, filepath2, style = 'stylish') => {
 
   const fileType = path.extname(filepath1);
 
-  let fileParsedData1 = '';
-  let fileParsedData2 = '';
-
-  if (fileType === 'json') {
-    fileParsedData1 = jsonParse(data1);
-    fileParsedData2 = jsonParse(data2);
-  } else {
-    fileParsedData1 = yamlParse(data1);
-    fileParsedData2 = yamlParse(data2);
-  }
+  const [fileParsedData1, fileParsedData2] = (fileType === 'json')
+    ? [jsonParse(data1), jsonParse(data2)]
+    : [yamlParse(data1), yamlParse(data2)];
 
   const comparsion = compareData(fileParsedData1, fileParsedData2);
 
